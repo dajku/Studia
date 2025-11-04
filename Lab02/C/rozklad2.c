6981da5570a68c2106f6e52995a76a56e6a2d3e9
Maciej <maciejzysk1@gmail.com>
2025-11-04 12:05:29 +0100

    Rozklad na liczby pierwsze w C bez tablic dynamicznych

--- Lab02/C/rozklad2.c
+++ Lab02/C/rozklad2.c
@@ -1,45 +1,68 @@
 #include <stdio.h>
 
-long long czynniki[32];
-int indeks = 0;
-void rozkladLiczby(long long n){
-    long long kopia_n = n;
-    while (kopia_n % 2 == 0){
-        czynniki[indeks] = 2;
-        indeks += 1;
-        kopia_n /= 2;
+long long czynniki[100];
+int ilosc = 0;
+
+void RozkladNaCzynniki(long long n){
+    long long kopiaN = n;
+    int index = 0;
+    while (kopiaN % 2 == 0){
+        czynniki[index] = 2;
+        kopiaN /= 2;
+        index += 1;
+        ilosc += 1;
     }
+    long long i = 3;
+    long long kwadrat = 9;
+    long long roznica = 16;
 
-    int i = 3;
-    int kwadrat = 9;
-    int roznica = 16;
-
-    while (kwadrat <= kopia_n){
-        while (kopia_n % i == 0){
-            czynniki[indeks] = i;
-            indeks += 1;
-            kopia_n /= i;
+    while (kwadrat <= kopiaN){
+        while(kopiaN % i == 0){
+            czynniki[index] = i;
+            index += 1;
+            ilosc += 1;
+            kopiaN /= i;
         }
         kwadrat += roznica;
         roznica += 8;
-        i += 2;
+        i += 2;  
+        
     }
-    if (kopia_n > 2){
-        czynniki[indeks] = kopia_n;
+    if (kopiaN > 2){
+        czynniki[index] = kopiaN;
+        ilosc += 1;
     }
-}
 
+}
 
 int main(){
     long long x;
-
     printf("Podaj liczbe: ");
     scanf("%lld", &x);
-    rozkladLiczby(x);
-    printf("%lld", x);
-    for (int i = 0; i <= indeks; i++)
-    {
-        printf("%lld\n", czynniki[i]);
+    RozkladNaCzynniki(x);
+    int i = 0;
+    printf("%lld=", x);
+    while (i < ilosc){
+        long long aktualnyCzynnik = czynniki[i];
+        int licznik = 0;
+        int j = i;
+
+        while(j < ilosc && aktualnyCzynnik == czynniki[j]){
+            licznik += 1;
+            j += 1;
+        }
+
+        if(i > 0){
+            printf("*");
+        }
+        printf("%lld", aktualnyCzynnik);
+
+        if(licznik > 1){
+            printf("^%d", licznik);
+        }
+        i += licznik;
+
     }
-    
+    printf("\n");
+    return 0;
 }
