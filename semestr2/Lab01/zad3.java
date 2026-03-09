25541d604095dfb85e9d8988c86845d61ca94023
Maciej <maciejzysk1@gmail.com>
2026-03-09 22:58:17 +0100

    semestr2

---
+++ semestr2/Lab01/zad3.java
@@ -0,0 +1,20 @@
+
+
+public class zad3{
+    
+    public static int div(int n){
+        if(n == 0){
+            System.out.println("Brak rozwiązania");
+        }
+        if(n % 2 == 0){
+            return n/2;
+        }
+        for(int i = 3; )
+
+
+        return 0;
+    }
+    public static void main(String[] args){
+        System.out.println();
+    }
+}
