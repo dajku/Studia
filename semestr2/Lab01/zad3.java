9fd41c0bb5677875a809adb239e7389f811fd3e8
Maciej <maciejzysk1@gmail.com>
2026-03-10 14:50:04 +0100

    lista 1

--- semestr2/Lab01/zad3.java
+++ semestr2/Lab01/zad3.java
@@ -4,17 +4,41 @@ public class zad3{
     
     public static int div(int n){
         if(n == 0){
-            System.out.println("Brak rozwiązania");
+            return -1;
+        }
+        if(n == 1){
+            return -1;
         }
         if(n % 2 == 0){
             return n/2;
         }
-        for(int i = 3; )
+        for(int i = 3; i*i <= n; i = i+2){
+            if(n % i == 0){
+                return n/i;
+            }
 
+        }
 
-        return 0;
+        return 1;
     }
     public static void main(String[] args){
-        System.out.println();
+        for (int i = 0; i < args.length; i++){
+            try {
+                int n = Integer.parseInt(args[i]); 
+                int wynik = div(n);
+                if(n < 0){
+                    System.out.println("Liczba ujemna");
+                }
+                else if(wynik == -1){
+                    System.out.println("Brak największego dzielnika");
+                }
+                else{
+                    System.out.println("Najwiekszy dzielnik " + n + " to " + wynik);
+                }
+            }
+            catch (NumberFormatException ex){
+                System.out.println(args[i] + " nie jest liczba calkowita");
+            }
+        }
     }
 }
