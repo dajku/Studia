4063db679832731e577cabda971e21e1d043f4f6
Maciej <maciejzysk1@gmail.com>
2026-04-16 20:32:43 +0200

    Zadanie 4 prawie

--- semestr2/Lab04/C++/Zad4.cpp
+++ semestr2/Lab04/C++/Zad4.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <iostream>
+#include <vector>
 #include <cmath>
 #include "Zad4.hpp"
 
@@ -45,7 +47,7 @@ std::string Pentagon::getName(){
     return "Pentagon";
 }
 
-Pentagon::Pentagon(){};
+Pentagon::~Pentagon(){};
 
 Hexagon::Hexagon(){};
 
@@ -92,6 +94,126 @@ Diamond::~Diamond(){};
 
 
 int main(int argc, char* argv[]){
+    if(argc == 1){
+        std::cout << "Nie podano argumentów" << "\n";
+        return 1;
+    }
+
+    std::vector<Figure::OneParameterCalc*> oneParamFigures;
+    std::vector<Figure::TwoParameterCalc*> TwoParamFigures;
+
+    int i = 1;
+    while (i < argc){
+        try{
+            std::string figure  = argv[i];
+            i++;
+            if(figure == "q" and std::stoi(argv[i+1]) == 90){
+                int side1 = std::stoi(argv[i]);
+                int side2 = std::stoi(argv[i]);
+                int side3 = std::stoi(argv[i]);
+                int side4 = std::stoi(argv[i]);
+                int angle = std::stoi(argv[i+1]);
+                i+=2;
+                if(side1 <= 0 || side2 <= 0 || side3 <= 0 || side4 <= 0){
+                    std::cout << "Nieprawidłowy bok dla: " << figure << "\n";
+                    continue;
+                }
+                if(side1 == side2 && side2 == side3 && side3 == side4 && angle == 90){
+                    Square f;
+                    std::cout << f.getName() << "\n";
+                    std::cout << "Area: " << f.calculateArea(side1) << "\n";
+                    std::cout << "Perimeter: "<< f.calculatePerimeter(side1) << "\n";
+
+                }
+                continue;
+
+            }
+            if(figure == "q"){
+                int side1 = std::stoi(argv[i]);
+                int side2 = std::stoi(argv[i+1]);
+                int side3 = std::stoi(argv[i+2]);
+                int side4 = std::stoi(argv[i+3]);
+                int angle = std::stoi(argv[i+4]);
+                i+=5;
+
+                if(side1 <= 0 || side2 <= 0 || side3 <= 0 || side4 <= 0){
+                    std::cout << "Nieprawidłowy bok dla: " << figure << "\n";
+                    continue;
+                }
+                if(side1 == side2 && side2 == side3 && side3 == side4 && angle == 90){
+                    Square f;
+                    std::cout << f.getName() << "\n";
+                    std::cout << "Area: " << f.calculateArea(side1) << "\n";
+                    std::cout << "Perimeter: "<< f.calculatePerimeter(side1) << "\n";
+                }
+                else if (side1 == side2 && side2 == side3 && side3 == side4){
+                    Diamond f;
+
+                    std::cout << f.getName() << "\n";
+                    std::cout << "Area: " << f.calculateArea(side1, angle) << "\n";
+                    std::cout << "Perimeter: "<< f.calculatePerimeter(side1, side2) << "\n";
+                }
+                else if(side1 == side2 && side3 == side4){
+                    Rectangle f;
+                    std::cout << f.getName() << "\n";
+                    std::cout << "Area: " << f.calculateArea(side1, side3) << "\n";
+                    std::cout << "Perimeter: "<< f.calculatePerimeter(side1, side3) << "\n";
+                }
+
+            }
+            else if(figure == "c"){
+                int radius = std::stoi(argv[i]);
+                i++;
+                if(radius <= 0){
+                    std::cout << "Nieprawidłowy promień dla: " << figure << "\n";
+                    continue;
+                }
+                Circle f;
+                std::cout << f.getName() << "\n";
+                std::cout << "Area: " << f.calculateArea(radius) << "\n";
+                std::cout << "Perimeter: "<< f.calculatePerimeter(radius) << "\n";
+            }
+            else if(figure == "p"){
+                int side = std::stoi(argv[i]);
+                i++;
+                if(side <= 0 ){
+                    std::cout << "Nieprawidłowy bok dla: " << figure << "\n";
+                    continue;
+                }
+                    Pentagon f;
+                    std::cout << f.getName() << "\n";
+                    std::cout << "Area: " << f.calculateArea(side) << "\n";
+                    std::cout << "Perimeter: "<< f.calculatePerimeter(side) << "\n";
+            }
+            else if(figure == "h"){
+                int side = std::stoi(argv[i]);
+                i++;
+                if(side <= 0){
+                    std::cout << "Nieprawidłowy bok dla " << figure << "\n";
+                    continue;
+                }
+                    Hexagon f;
+                    std::cout << f.getName() << "\n";
+                    std::cout << "Area: " << f.calculateArea(side) << "\n";
+                    std::cout << "Perimeter: "<< f.calculatePerimeter(side) << "\n";
+
+            }
+        }
+        catch(const std::invalid_argument& ex){
+            std::cout << "Nieprawidłowa dana: " << ex.what() << "\n";
+            continue;
+        }
+        catch(const std::out_of_range& ex){
+            std::cout << "Za mało parametrów" << "\n";
+            continue;
+        } 
+        catch(const std::logic_error& ex){
+            std::cout << "Błędne podanie parametrów" << "\n";
+            continue;
+        }
+        
+    }
+
 
 }
 
