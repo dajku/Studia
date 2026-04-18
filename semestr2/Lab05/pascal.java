52a4107e6a8f64e53a9cb08fc7aff6c2ecfbe4d8
Maciej <maciejzysk1@gmail.com>
2026-04-18 16:59:53 +0200

    zad1

--- semestr2/Lab05/pascal.java
+++ semestr2/Lab05/pascal.java
@@ -2,86 +2,79 @@ import java.awt.*;
 
 import javax.swing.*;
 
-
-
 public class pascal {
-    
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        if(args.length == 0){
+        if (args.length == 0) {
             System.out.println("Nie podano argumentów");
             return;
         }
-        try{
+        try {
             int n = Integer.parseInt(args[0]);
-            if(n <= 0){
+            if (n <= 0) {
                 System.out.println("Błędny argument");
                 return;
             }
 
             JFrame fr = new JFrame("Pascal");
 
-            fr.setSize(1000,1000);
+            fr.setSize(1000, 1000);
 
             fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            
-            fr.setVisible(true);
-
 
             JPanel mainPanel = new JPanel();
             mainPanel.setBackground(Color.BLACK);
 
             mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
-
             fr.setContentPane(mainPanel); // ustawienie mainPanel jako główny canvas
 
             mainPanel.add(Box.createVerticalGlue()); // dodanie górnego marginesu
             Font mainFont = new Font("Arial", Font.BOLD, 24);
 
-
             // IMPLEMENTACJA NIEOPTYMALNA (czasowo) czyli z użyciem klasy PascalTriangleRow
-            // Oblicza każdy wiersz po kolei aż do n, więc powtarzają sie obliczenia ale wykorzystuje już napisany kod
-            for(int i = 0; i <= n; i++){
+            // Oblicza każdy wiersz po kolei aż do n, więc powtarzają sie obliczenia ale
+            // wykorzystuje już napisany kod
+            for (int i = 0; i <= n; i++) {
 
                 JPanel triangleRow = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
                 triangleRow.setBackground(Color.BLACK);
 
-                PascalTriangleRow calcPascal = new PascalTriangleRow(i); 
-                for(int j = 0; j < i + 1; j++){
+                PascalTriangleRow calcPascal = new PascalTriangleRow(i);
+                for (int j = 0; j < i + 1; j++) {
                     int value = calcPascal.pascalValue(j);
-                    if(value != 0){
-                        JLabel number = new JLabel(String.valueOf(value)); // W konstruktorze powinna być liczba obliczona z trójkąta pascala;
-                        
+                    if (value != 0) {
+                        JLabel number = new JLabel(String.valueOf(value)); // W konstruktorze powinna być liczba
+                                                                           // obliczona z trójkąta pascala;
+
                         number.setFont(mainFont);
                         number.setForeground(Color.WHITE);
-                        number.setPreferredSize(new Dimension(50, 50));
+                        number.setPreferredSize(new Dimension(75, 75));
                         number.setHorizontalAlignment(SwingConstants.CENTER);
 
                         triangleRow.add(number);
                     }
 
-
                 }
-                triangleRow.setMaximumSize(new Dimension(Integer.MAX_VALUE, triangleRow.getHeight()));
+                triangleRow.setMaximumSize(new Dimension(Integer.MAX_VALUE, triangleRow.getPreferredSize().height)); // ustawienie aby wiersze mogły brać całą szerokość ekranu ale żeby wysokość zostawała taka sama
                 mainPanel.add(triangleRow);
             }
             mainPanel.add(Box.createVerticalGlue()); // dodanie dolnego marginesu
+
+            fr.setVisible(true);
             mainPanel.setVisible(true);
 
-        }
-        catch(InvalidArraySize ex){
+        } catch (InvalidArraySize ex) {
             System.out.println(ex + ": " + args[0] + " - Nieprawidłowy wiersz trójkąta Pascala");
             return;
 
-        }
-        catch(NumberFormatException ex){
+        } catch (NumberFormatException ex) {
             System.out.println(args[0] + " - nieprawidłowa dana");
             return;
         }
 
     }
-    
+
 }
