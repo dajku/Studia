b683328f98204f49f892102d7681184860bc4978
Maciej <maciejzysk1@gmail.com>
2026-04-23 17:30:48 +0200

    Lista skonczona

--- semestr2/Lab05/CGui.java
+++ semestr2/Lab05/CGui.java
@@ -12,10 +12,8 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Font;
 
