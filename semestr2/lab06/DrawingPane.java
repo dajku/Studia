55d86e199c468f0b72dd8d3e783245f8869d23a2
Maciej <maciejzysk1@gmail.com>
2026-05-10 19:37:34 +0200

    cos tam

--- semestr2/lab06/DrawingPane.java
+++ semestr2/lab06/DrawingPane.java
@@ -17,75 +17,64 @@ public class DrawingPane extends Pane{
     
 
     public DrawingPane(){
-        if(currentMode == ToolMode.RECTANGLE){
 
             setOnMousePressed(e ->{
                 startX = e.getX();
                 startY = e.getY();
 
-                currentRectangle = new Rectangle();
-                currentRectangle.setX(startX);
-                currentRectangle.setY(startY);
-                currentRectangle.setWidth(0);
-                currentRectangle.setHeight(0);
-                currentRectangle.setFill(Color.LIGHTBLUE);
-                currentRectangle.setStroke(Color.BLACK);
+                if(currentMode == ToolMode.RECTANGLE){
 
-                getChildren().add(currentRectangle);
+
+                    currentRectangle = new Rectangle();
+                    currentRectangle.setX(startX);
+                    currentRectangle.setY(startY);
+                    currentRectangle.setWidth(0);
+                    currentRectangle.setHeight(0);
+                    currentRectangle.setFill(Color.LIGHTBLUE);
+                    currentRectangle.setStroke(Color.BLACK);
+
+                    getChildren().add(currentRectangle);
+                }
+
+                else if(currentMode == ToolMode.CIRCLE){
+                    currentCircle = new Circle();
+                    currentCircle.setCenterX(startX);
+                    currentCircle.setCenterY(startY);
+                    currentCircle.setRadius(0);
+                    currentCircle.setFill(Color.LIGHTBLUE);
+                    currentCircle.setStroke(Color.BLACK);
+                    getChildren().add(currentCircle);
+                }
             });
 
             setOnMouseDragged(e -> {
-                double currentX = e.getX();
-                double currentY = e.getY();
+                if(currentMode == ToolMode.RECTANGLE && currentRectangle != null){
+                    double x = Math.min(startX, e.getX());
+                    double y = Math.min(startY, e.getY());
+                    double width = Math.abs(e.getX() - startX);
+                    double height = Math.abs(e.getY() - startY);
+                    currentRectangle.setX(x);
+                    currentRectangle.setY(y);
+                    currentRectangle.setWidth(width);
+                    currentRectangle.setHeight(height);
+                }
+                else if(currentMode == ToolMode.CIRCLE && currentCircle != null){
+                    double radius = Math.sqrt(
+                        Math.pow(e.getX() - startX, 2) + Math.pow(e.getY() - startY, 2)
+                    );
+                    currentCircle.setRadius(radius);
+                }
 
-                double x = Math.min(startX, currentX);
-                double y = Math.min(startY, currentY);
-                double width = Math.abs(currentX - startX);
-                double height = Math.abs(currentY - startY);
 
-                currentRectangle.setX(x);
-                currentRectangle.setY(y);
-                currentRectangle.setWidth(width);
-                currentRectangle.setHeight(height);
             });
 
             setOnMouseReleased(e -> {
                 currentRectangle = null;
-            });
-        }
-        else if(currentMode == ToolMode.CIRCLE){
-
-            setOnMousePressed(e ->{
-                startX = e.getX();
-                startY = e.getY();
-
-                currentCircle = new Circle();
-                currentCircle.setCenterX(startX);
-                currentCircle.setCenterY(startY);
-                currentCircle.setFill(Color.LIGHTBLUE);
-                currentCircle.setStroke(Color.BLACK);
-
-                getChildren().add(currentCircle);
-            });
-
-            setOnMouseDragged(e -> {
-                double currentX = e.getX();
-                double currentY = e.getY();
-
-                double x = Math.min(startX, currentX);
-                double y = Math.min(startY, currentY);
-                double radius = Math.abs(Math.sqrt((currentX - startX)*(currentX - startX) + (currentY - startY)*(currentY - startY)));
-
-                currentCircle.setCenterX(x);
-                currentCircle.setCenterY(y);
-                currentCircle.setRadius(radius);
-            });
-
-            setOnMouseReleased(e -> {
                 currentCircle = null;
             });
+        
         }
-    }
+    
 
     public void setToolMode(ToolMode mode){
         currentMode = mode;
