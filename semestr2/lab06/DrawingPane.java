1966af97a956ca29374263263c43212e75fbd2fb
Maciej <maciejzysk1@gmail.com>
2026-05-11 21:16:33 +0200

    1 i 2 podpunkt zrobiony

--- semestr2/lab06/DrawingPane.java
+++ semestr2/lab06/DrawingPane.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
@@ -11,48 +13,103 @@ public class DrawingPane extends Pane{
 
     private double startX;
     private double startY;
+    private double lastMouseX;
+    private double lastMouseY;
     private ToolMode currentMode;
     private Rectangle currentRectangle;
     private Circle currentCircle;
+    private Polygon currentPolygon;
+    private ArrayList<Shape> allFigures;
+    private Shape currentShape;
     
 
     public DrawingPane(){
 
+            
+
             setOnMousePressed(e ->{
                 startX = e.getX();
                 startY = e.getY();
+                lastMouseX = startX;
+                lastMouseY = startY;
+                Object target = e.getTarget();
+                if(target instanceof Shape){
+                    currentShape = (Shape) target; // sprawdziłem Shape, rzutuję Shape na target
+                    currentShape.setStrokeWidth(3);
+                    setOnScroll(s -> {
+                        if(s.isShiftDown()){
+                            currentShape.setRotate(currentShape.getRotate() + s.getDeltaY()*0.1 + s.getDeltaX()*0.1);
+                        }
+                        else{
+                            currentShape.setScaleX(currentShape.getScaleX() + s.getDeltaY()*0.0005);
+                            currentShape.setScaleY(currentShape.getScaleY() + s.getDeltaY()*0.0005);
+                        }
 
-                if(currentMode == ToolMode.RECTANGLE){
-
-
-                    currentRectangle = new Rectangle();
-                    currentRectangle.setX(startX);
-                    currentRectangle.setY(startY);
-                    currentRectangle.setWidth(0);
-                    currentRectangle.setHeight(0);
-                    currentRectangle.setFill(Color.LIGHTBLUE);
-                    currentRectangle.setStroke(Color.BLACK);
-
-                    getChildren().add(currentRectangle);
+                    });
                 }
+                else{
+                    if(currentShape != null){
+                        currentShape.setStrokeWidth(1);
+                    }
+                    currentShape = null;
+                    if(currentMode == ToolMode.RECTANGLE){
 
-                else if(currentMode == ToolMode.CIRCLE){
-                    currentCircle = new Circle();
-                    currentCircle.setCenterX(startX);
-                    currentCircle.setCenterY(startY);
-                    currentCircle.setRadius(0);
-                    currentCircle.setFill(Color.LIGHTBLUE);
-                    currentCircle.setStroke(Color.BLACK);
-                    getChildren().add(currentCircle);
+
+                        currentRectangle = new Rectangle();
+
+                        currentRectangle.setX(startX);
+                        currentRectangle.setY(startY);
+
+                        currentRectangle.setWidth(0);
+                        currentRectangle.setHeight(0);
+
+                        currentRectangle.setFill(Color.LIGHTBLUE);
+                        currentRectangle.setStroke(Color.BLACK);
+                        getChildren().add(currentRectangle);
+                    }
+
+                    else if(currentMode == ToolMode.CIRCLE){
+                        currentCircle = new Circle();
+
+                        currentCircle.setCenterX(startX);
+                        currentCircle.setCenterY(startY);
+
+                        currentCircle.setRadius(0);
+
+                        currentCircle.setFill(Color.LIGHTBLUE);
+                        currentCircle.setStroke(Color.BLACK);
+
+                        getChildren().add(currentCircle);
+                    }
+                    else if(currentMode == ToolMode.POLYGON){
+                        if(currentPolygon == null){
+                            currentPolygon = new Polygon();
+                            currentPolygon.setFill(Color.LIGHTBLUE);
+                            currentPolygon.setStroke(Color.BLACK);
+                            currentPolygon.getPoints().addAll(startX, startY);
+
+                            getChildren().add(currentPolygon);
+
+                        }
+                        else{
+                            currentPolygon.getPoints().addAll(e.getX(), e.getY());
+                        }
+
+                    }
                 }
             });
 
+
+
             setOnMouseDragged(e -> {
+                double currentX = e.getX();
+                double currentY = e.getY();
                 if(currentMode == ToolMode.RECTANGLE && currentRectangle != null){
-                    double x = Math.min(startX, e.getX());
-                    double y = Math.min(startY, e.getY());
-                    double width = Math.abs(e.getX() - startX);
-                    double height = Math.abs(e.getY() - startY);
+                    double x = Math.min(startX, currentX);
+                    double y = Math.min(startY, currentY);
+
+                    double width = Math.abs(currentX - startX);
+                    double height = Math.abs(currentY - startY);
                     currentRectangle.setX(x);
                     currentRectangle.setY(y);
                     currentRectangle.setWidth(width);
@@ -60,9 +117,23 @@ public class DrawingPane extends Pane{
                 }
                 else if(currentMode == ToolMode.CIRCLE && currentCircle != null){
                     double radius = Math.sqrt(
-                        Math.pow(e.getX() - startX, 2) + Math.pow(e.getY() - startY, 2)
+                        Math.pow(currentX - startX, 2) + Math.pow(currentY - startY, 2)
                     );
                     currentCircle.setRadius(radius);
+                }
+                
+                if(currentShape != null){
+                    
+                    double currentSceneX = e.getSceneX();
+                    double currentSceneY = e.getSceneY();
+                    double deltaX = currentSceneX - lastMouseX;
+                    double deltaY = currentSceneY - lastMouseY;
+
+                    currentShape.setTranslateX(currentShape.getTranslateX() + deltaX);
+                    currentShape.setTranslateY(currentShape.getTranslateY() + deltaY);
+                    lastMouseX = currentSceneX;
+                    lastMouseY = currentSceneY;
+
                 }
 
 
@@ -71,6 +142,10 @@ public class DrawingPane extends Pane{
             setOnMouseReleased(e -> {
                 currentRectangle = null;
                 currentCircle = null;
+
+                if(e.getClickCount() == 2){
+                    currentPolygon = null;
+                }
             });
         
         }
